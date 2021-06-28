e9a0ea47c1a41c2842f2031c55b8c5835849ee81
tahanialzubaidi <86547193+tahanialzubaidi@users.noreply.github.com>
2021-06-28 06:17:30 +0300

    Add files via upload

---
+++ motors.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 28, 2021 at 03:59 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `motors`
+--
+
+CREATE TABLE `motors` (
+  `motor1` int(3) NOT NULL,
+  `motor2` int(11) NOT NULL,
+  `motor3` int(11) NOT NULL,
+  `motor4` int(11) NOT NULL,
+  `motor5` int(11) NOT NULL,
+  `motor6` int(11) NOT NULL,
+  `on` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `motors`
+--
+
+INSERT INTO `motors` (`motor1`, `motor2`, `motor3`, `motor4`, `motor5`, `motor6`, `on`) VALUES
+(30, 160, 100, 70, 178, 5, NULL),
+(30, 160, 100, 70, 178, 5, NULL),
+(40, 50, 30, 120, 140, 150, NULL);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
